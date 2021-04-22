3f53b9d8b4c95898fc640dc9766aa39e80c2546a
JW <wenroujing@hotmail.com>
2021-04-22 17:30:00 -0400

    basic search api and pagestyle done

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,70 @@
-import React from 'react';
-import { 
-  BrowserRouter as Router, 
-  Switch, 
-  Route, 
-  Link,
-  NavLink
-} from 'react-router-dom';
-import PostsListPage from './pages/PostsListPage';
-import PostFormPage from './pages/PostFormPage';
-import ShowPostPage from './pages/ShowPostPage';
-import AboutUsPage from './pages/AboutUsPage';
+import React, {useEffect, useState} from "react"; 
+import Recipe from "./Recipe";
+import "./App.css";
 
-import './App.css';
+const App = () => {
+  const APP_ID = "b4fcc1ef";
+  const APP_KEY = "1ab95ad0d60a68dacbc5ae6e6c93a00b";
 
+  //create a state set it to an array of objects
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken');//only when we hit search then can we search for data
 
-function Navigation(props) {
-  return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-      <Link className="navbar-brand" to="/">Micro Blog</Link>
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <NavLink className="nav-link" exact to="/posts/new">
-            Create a Micro Post
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" exact to="/about-us">
-            About Us
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
 
-
-class App extends React.Component {
-  render() {
-    return (
-        <Router>
-          <Navigation />
-          <div className="container-fluid text-center">
-            <div className="row justify-content-center">
-              <Switch>
-                <Route path="/posts/new" component={PostFormPage} />
-                <Route path="/posts/:id" component={ShowPostPage} />
-                <Route path="/about-us" component={AboutUsPage} />
-                <Route path="/" component={PostsListPage} />
-              </Switch>
-            </div>
-          </div>
-        </Router>
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = e => {
+    //access the target from this event
+    setSearch(e.target.value);
+    //console.log(search);
   }
-}
+  //search on the form submit
+  const getSearch = e => {
+    e.preventDefault(); //stop the page refresh
+    setQuery(search);
+    setSearch('');
+  }
+
+  return(
+    <div className="App">
+      
+        <form onSubmit={getSearch} className="seach-form">
+          <input 
+          className="search-bar" 
+          type="text" 
+          value={search} 
+          onChange = {updateSearch} 
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+
+        <div className={recipes}>
+        {recipes.map(recipe => (
+          <Recipe 
+            key={recipe.recipe.label}
+            title={recipe.recipe.label} 
+            calories={recipe.recipe.calories} 
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+        </div>
+      
+    </div>
+  );
+};
 
 
 export default App;
