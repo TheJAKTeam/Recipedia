4bab9936137209065f6a37199963c32e0ae38e34
Kevin Tam <kevin@kevintamcs.com>
2021-05-08 01:27:44 -0400

    Remove unused toast import in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 import Routes from "./routes";
 import Navbar from "./components/Navbar/Navbar";
